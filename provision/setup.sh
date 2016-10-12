7327af7fa251f7b180f5a51b8a7409b6e15b08f4
Michael Kelly <jcisecurebuildinfo@gmail.com>
2016-10-11 22:24:41 -0700

    Replaced damaged file that was inadvertantly committed this afternoon

--- provision/setup.sh
+++ provision/setup.sh
@@ -3,13 +3,29 @@
 ## Vagrant and Docker sometimes work better together if the user is in a docker group. Create the group.
 
 # Creating Docker group and adding user to it if not done so already
-groupadd docker > /dev/null 2>&1
+if grep -q "^docker:" /etc/group
+then
+  echo "Adding docker group"
+  groupadd docker > /dev/null 2>&1
+fi
 usermod -aG docker $USER > /dev/null 2>&1
+
+#groupadd nobody > /dev/null 2>&1
+#usermod -aG nobody $USER > /dev/null 2>&1
+
+#sudo mkdir -p /usr/share/nginx/html/
 
 # This program extracts necessary information from git to determine both the docker files and the web files served. Install git.
 
-# Installing Git
-apt-get install git -y > /dev/null 2>&1
+# Check if git is installed, if not, install.
+if apt -q list installed git
+  then
+    echo "Git already installed"
+  else
+    echo "Installing Git"
+    sudo apt-get install git -y > /dev/null 2>&1
+  fi
+sudo apt-get install git -y
 
 # I like to know when the startup script is finished. It makes debugging easier later if we need to make changes.
 
